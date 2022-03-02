7409c27fda7a95cf3ab2d1db670c99b7a7caa98d
saadu1998 <1998saadullah@gmail.com>
2022-03-02 22:30:50 +0500

    changed

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const key= 'ANXRNGQpTlTeUtbuB94XEDMRl8XoZXe4';
 // to get the city api 
 const getCity = async (city) =>{
 
-    const baseURL = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    const baseURL = 'https://dataservice.accuweather.com/locations/v1/cities/search';
     const querry = `?apikey=${key}&q=${city}`;
 
     const response = await fetch (baseURL + querry) ;
@@ -21,7 +21,7 @@ const getCity = async (city) =>{
 // weather condition api
 const getCondition = async (id) =>{
 
-    const baseURL = 'http://dataservice.accuweather.com/currentconditions/v1/';
+    const baseURL = 'https://dataservice.accuweather.com/currentconditions/v1/';
     const querry = `${id}?apikey=${key}`;
 
     const response = await fetch(baseURL + querry);
